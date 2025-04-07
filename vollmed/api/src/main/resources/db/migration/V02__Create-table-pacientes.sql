c08654c7bffb2c5a045b466900ac0a706bbffb0a
Nicksemery <164393173+Nicksemery@users.noreply.github.com>
2025-04-07 10:35:15 -0300

    Update V02__Create-table-pacientes.sql

--- vollmed/api/src/main/resources/db/migration/V02__Create-table-pacientes.sql
+++ vollmed/api/src/main/resources/db/migration/V02__Create-table-pacientes.sql
@@ -1,4 +1,4 @@
-create table medicos(
+create table pacientes(
                         id int not null auto_increment,
                         nome varchar(100) not null,
                         email varchar(100) not null unique,
